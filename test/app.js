302d7ba9d0729c3e05991da30bb34525677ba7d2
Josué Hoenicka <josuee.vzla@gmail.com>
2023-05-19 16:42:35 -0300

    create test.js / update app.js

--- test/app.js
+++ test/app.js
@@ -1,6 +1,12 @@
 const {saludar, saludarJosue} = require('./saludos.js')
 
 // console.log(saludos.saludar('Josue'));
-
 console.log(saludar("Josue"));
 console.log(saludarJosue());
+
+// console.warn("Hola mundo");
+// console.error(new Error("Ups!")); 
+// console.log(process);
+// console.log(process.env);
+// console.log(process.argv);
+console.log(process.memoryUsage());
